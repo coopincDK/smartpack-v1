01e37929af4d2c3e527fef4ccb38ffbdceb99f63
coopincDK <mrm@smartpack.dk>
2026-05-03 01:47:07 +0200

    fix: top banner ny tekst + mailto link

--- js/header.js
+++ js/header.js
@@ -16,7 +16,7 @@
     var html = [
       '<div class="site-announce" id="site-announce">',
       '  <span class="site-announce__inner">',
-      '    <span>Brug for et Bundlinjeboost? Skriv til <a href="mailto:bundlinjeboost@smartpack.dk" class="site-announce__link">bundlinjeboost@smartpack.dk</a></span>',
+      '    <span>Brug for et løft på bundlinjen? Det starter typisk på lageret. <a href="mailto:bundlinjeboost@smartpack.dk" class="site-announce__link">Lad os se på jeres setup &rarr;</a></span>',
       '  </span>',
       '  <button class="site-announce__close" id="site-announce-close" aria-label="Luk">&#x2715;</button>',
       '</div>',
